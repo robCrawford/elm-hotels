67e8543c6f782c3a98423015747f94505c9d2d3e
rcrawford <rcrawford@travelrepublic.co.uk>
2017-02-20 12:50:45 +0000

    Results layout

--- src/Hotels.elm
+++ src/Hotels.elm
@@ -8,6 +8,7 @@ import Http
 import Json.Decode as Json
 
 
+main : Program Never Model Msg
 main =
     Html.program
         { init = init
@@ -23,7 +24,7 @@ main =
 
 type alias Hotel =
     { name : String
-    , thumbnailUrl : String
+    , imageUrl : String
     }
 
 
@@ -92,23 +93,153 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ input [ placeholder "First Name", onInput FirstName ] []
-        , input [ placeholder "Last Name", onInput LastName ] []
-        , button [ onClick (Logo "img/logo.png") ] [ text "Load logo" ]
-        , div [] [ text (String.concat [ model.firstName, model.lastName ]) ]
-        , img [ src model.imgUrl ] []
-        , br [] []
-        , br [] []
-        , div [] (List.map hotelView model.hotels)
+    -- div [ class "container main-container" ]
+    --     [ div
+    --         [ class "row" ]
+    --         [ input [ placeholder "First Name", onInput FirstName ] []
+    --         , input [ placeholder "Last Name", onInput LastName ] []
+    --         , button [ onClick (Logo "img/logo.png") ] [ text "Load logo" ]
+    --         , div [] [ text (String.concat [ model.firstName, model.lastName ]) ]
+    --         , img [ src model.imgUrl ] []
+    --         , div
+    --             [ id "results"
+    --             , class "row hotelProduct xsResponse clearfix"
+    --             ]
+    --             (List.map hotelView model.hotels)
+    --         ]
+    --     ]
+    div [ class "container main-container" ]
+        [ div [ class "row" ]
+            [ div [ class "col-lg-12" ]
+                [ ul [ class "breadcrumb" ]
+                    [ li []
+                        [ a [ href "index.html" ]
+                            [ text "Home" ]
+                        ]
+                    , li [ class "active" ]
+                        [ text "HOTELS" ]
+                    ]
+                ]
+            ]
+        , div [ class "row" ]
+            [ div [ class "col-lg-3 col-md-3 col-sm-12" ]
+                [ div [ class "panel-group" ]
+                    [ div [ class "panel panel-default" ]
+                        [ div [ class "panel-heading" ]
+                            [ h4 [ class "panel-title" ]
+                                [ text "Filters" ]
+                            ]
+                        , div [ class "panel-body" ]
+                            [ ul [ class "filters" ]
+                                [ li []
+                                    [ text "Distance"
+                                    , input [ id "filter-distance" ]
+                                        []
+                                    ]
+                                , li []
+                                    [ text "Name"
+                                    , input [ id "filter-name" ]
+                                        []
+                                    ]
+                                , li []
+                                    [ text "Stars"
+                                    , input [ id "filter-stars" ]
+                                        []
+                                    ]
+                                , li []
+                                    [ text "Rating"
+                                    , input [ id "filter-rating" ]
+                                        []
+                                    ]
+                                , li []
+                                    [ text "Minimum price"
+                                    , input [ id "filter-price" ]
+                                        []
+                                    ]
+                                ]
+                            , div []
+                                [ a [ class "btn btn-primary" ]
+                                    [ text "Update" ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            , div [ class "col-lg-9 col-md-9 col-sm-12" ]
+                [ div [ class "clearfix hotel-header" ]
+                    [ h2 []
+                        [ text "HOTELS" ]
+                    ]
+                , div [ class "hotelFilter clearfix" ]
+                    [ div [ class "pull-right" ]
+                        [ div [ class "change-order pull-right" ]
+                            [ select [ class "form-control", id "update-sort" ]
+                                [ option [ value "distance-asc" ]
+                                    [ text "Sort by distance" ]
+                                , option [ value "name-asc" ]
+                                    [ text "Sort by name" ]
+                                , option [ value "stars-desc" ]
+                                    [ text "Sort by stars: high to low" ]
+                                , option [ value "stars-asc" ]
+                                    [ text "Sort by stars: low to high" ]
+                                , option [ value "rating-desc" ]
+                                    [ text "Sort by rating: high to low" ]
+                                , option [ value "rating-asc" ]
+                                    [ text "Sort by rating: low to high" ]
+                                , option [ value "price-desc" ]
+                                    [ text "Sort by price: high to low" ]
+                                , option [ value "price-asc" ]
+                                    [ text "Sort by price: low to high" ]
+                                ]
+                            ]
+                        , div [ class "change-view pull-right" ]
+                            [ a [ title "Grid", class "grid-view" ]
+                                [ i [ class "fa fa-th-large" ] [] ]
+                            , a [ title "List", class "list-view " ]
+                                [ i [ class "fa fa-th-list" ] [] ]
+                            ]
+                        ]
+                    ]
+                , div [ class "row hotelProduct xsResponse clearfix", id "results" ]
+                    (List.map hotelView model.hotels)
+                , div [ class "pagination pull-right no-margin-top" ]
+                    [ ul [ class "pagination no-margin-top" ]
+                        [ li [] [ a [] [ text "«" ] ]
+                        , li [ class "active" ] [ a [] [ text "1" ] ]
+                        , li [] [ a [] [ text "2" ] ]
+                        , li [] [ a [] [ text "3" ] ]
+                        , li [] [ a [] [ text "4" ] ]
+                        , li [] [ a [] [ text "5" ] ]
+                        , li [] [ a [] [ text "»" ] ]
+                        ]
+                    ]
+                ]
+            ]
         ]
 
 
 hotelView : Hotel -> Html Msg
 hotelView hotel =
-    div [ class "hotel" ]
-        [ img [ src hotel.thumbnailUrl ] []
-        , div [ class "content" ] [ (text hotel.name) ]
+    div [ class "item col-sm-4 col-lg-4 col-md-4 col-xs-6" ]
+        [ div [ class "product" ]
+            [ div [ class "image" ]
+                [ img
+                    [ src hotel.imageUrl
+                    , class "img-responsive"
+                    ]
+                    []
+                ]
+            ]
+        , div [ class "description" ]
+            [ ul []
+                [ li [ class "hotel-title" ]
+                    [ (text hotel.name)
+                    ]
+                ]
+            , div []
+                [ a [ class "btn btn-primary" ] [ text "More info" ]
+                ]
+            ]
         ]
 
 
@@ -148,4 +279,4 @@ mapEstablishments =
     --       (a -> value) -> Decoder a -> Decoder value
     Json.map2 Hotel
         (Json.field "Name" Json.string)
-        (Json.field "ThumbnailUrl" Json.string)
+        (Json.field "ImageUrl" Json.string)
